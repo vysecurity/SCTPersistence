d020147285f85f011e80c7001400987ed64e6bbf
Casey Smith <netsmith@gmail.com>
2016-04-13 05:07:52 -0500

    Added files via upload

---
+++ TestJS.js
@@ -0,0 +1,3 @@
+var ref = new ActiveXObject("Component.InsideCOMJS");
+var x = ref.Sum(4,6);
+WScript.Echo(x);
---
+++ TestVB.vbs
@@ -0,0 +1,8 @@
+Dim ref 
+
+Set ref = CreateObject("Component.InsideCOM")
+
+MsgBox ref.Sum(4, 6)
+
+
+	
